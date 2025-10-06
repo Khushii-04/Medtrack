d13cd9719f9a7707196bdf1aaee1595d4fd97d55
Aanchalkanwar <aanchalkanwar25@gmail.com>
2025-10-06 10:04:02 +0530

    Add files via upload

---
+++ dbConnection.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+const dbConnection = () => {
+    mongoose.connect(process.env.MONGO_URI,{
+        dbName: "RESTAURANT"
+    }).then(() => {
+        console.log("Connect to database successfully")
+    }).catch(err=>{
+        console.log(`Some error occured during connection ${err}`);
+    })
+}
+
+export default dbConnection;
